3dab73a9bdaade44a1f49e3b0ba19e3e7905d3b5
SaarthakTuli <saarthaktuli@gmail.com>
2022-10-15 22:57:08 +0530

    init

--- jyotimetallics/src/App.jsx
+++ jyotimetallics/src/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import
+{
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+
 import Home from './HomePage/Home'
+import About from './About/About'; 
+import Products from './Products/Products';
+import Contact from './Contact/Contact';
 
 import './index.css'
 
 function App() {
     return (
-        <Home />
+        <Router>
+            <Switch>
+                    <Route index element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Switch>
+        </Router>
     );
 }
 
