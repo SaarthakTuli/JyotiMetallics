ac26d0afe21982d797dc59fe13bce5e29364c574
SaarthakTuli <saarthaktuli@gmail.com>
2022-10-15 19:15:53 +0530

    init

--- jyotimetallics/src/HomePage/Header/Header.jsx
+++ jyotimetallics/src/HomePage/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Banner from '../../assets/Banner1.jpg'
 
 function Header() {
   return (
-      <header>
+      <header className='banner'>
+              <img
+                  className='banner__img'
+                  src={Banner}
+                  alt="Banner-1"
+          />
           
+          <div className='banner__contents'>
+              <h1 className='banner__title'>Welcome to Jyoti Metallics</h1>
+              <button className='banner__button'>Know More</button>
+          </div>
+
     </header>
   )
 }
