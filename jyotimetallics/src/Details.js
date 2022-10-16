3df5e97480319c8c1dc093aef1754b291126b66a
SaarthakTuli <saarthaktuli@gmail.com>
2022-10-17 01:25:06 +0530

    init

--- jyotimetallics/src/Details.js
+++ jyotimetallics/src/Details.js
@@ -6,4 +6,32 @@ details = [
       "We are considered the most reliable company because we are committed to providing our customers with the CR/HR coil slitter line. This CR/HR coil slitter line is used in different industries to cut different metals. Our product range includes different ranges such as pull-through, single-loop, double-loop, etc. to meet your specific needs, depending on your requirements. The capacity of this machine can be expanded from 300mm to 2000mm strip width, 0.1mm to 10mm strip thickness and 3MT to 35MT coil weight.",
     image: "images/CR.jpg",
   },
+  {
+    id: 2,
+    name: "CR/ HR Cut-to-Length",
+    description:
+      "CR cutting line CR Cut-to-Length Line machines are used to decoil and straighten sheet metal and automatically supply or cut sheet metal to the required length. Our products ensure that the process is accurate and better shearing is done on the processing line. This product can be used in a range of width 2000mm and sheet thickness  0.5 to 4.0 mm. This product is also used in cold and hot rolling of carbon steel, as well as in all types of metal materials with surface coatings. This machine is used for automatic feeding and cutting of metal sheets of exact length. ReCoiler / Uncoiler and straightener Decorator and straighteners are known for their best coil straightener services. This machine was designed with the important property of rewinding at different speeds in mind. The impeccable performance and high functionality are highly valued. This machine is modeled and designed using the latest technology to reduce price and increase efficiency. This machine is needed for seamless applications in the automotive industry.",
+    image: "images/HRCutToLength.jpg",
+  },
+  {
+    id: 3,
+    name: "Pickling Line",
+    description:
+      "The metal surface may contain contaminants that may affect future use or further treatment of the product. To remove these impurities, carbon steel with an alloy content of 6% or less is soaked in hydrochloric acid or sulfuric acid. Steels with a high alloy content use other acids (such as nitric acid and phosphoric acid) and must be pickled in two steps. Pickling lines usually follow the hot rolling section of metal manufacturing. As the coiled metal leaves the mill, it is unwound and passes through a series of tanks filled with pickled acid and rinse tanks. If necessary, it is further processed in a cold rolling mill.(Devander Ji will provide pics).",
+    image: "images/PicklingLine.jpg",
+  },
+  {
+    id: 4,
+    name: "Continuous galvanizing line (CGL) ",
+    description:
+      "The purpose of the continuous galvanizing line (CGL) is to apply a layer of zinc to the surface of the steel sheet to improve corrosion resistance. The thickness of the zinc layer is widely controlled, and galvanized steel sheets are used in a wide range of applications such as building materials, automobiles, and electrical appliances. CGL has unparalleled features that are highly valued by customers.",
+    image: "images/CGL.jpg",
+  },
+  {
+    id: 5,
+    name: "Continuous galvanizing line (CGL) ",
+    description:
+      "Product Description Promising outstanding quality, the company was able to provide its valued customers with the most affordable quality support roller blocks. The reel inductors offered are manufactured in our advanced production units in accordance with established industry standards and under the strict supervision of qualified professionals. The retractors we offer are highly regarded in the market for their high performance and rugged construction. Customers can purchase these products at marginal prices. Characteristic: Low maintenance Extension of life Sturdy structure. Product Description: With market development in mind, we manufacture, export and supply the highest quality backup roll chock. We develop high-quality work rolls and backup rolls that are active in various industries. Known for its reliable performance, compact design and low maintenance requirements, our chokes are available to our customers in stock sizes from 200mm to 800mm. Our colors are also tested with various parameters by quality controllers to ensure their performance and durability. (can be customised according to customer need)",
+    image: "images/BackupChocks.jpg",
+  },
 ];
