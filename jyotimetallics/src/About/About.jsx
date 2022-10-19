6feb227cf9bfac00deef63d5320e1b37ec213794
SaarthakTuli <saarthaktuli@gmail.com>
2022-10-20 00:36:48 +0530

    init

--- jyotimetallics/src/About/About.jsx
+++ jyotimetallics/src/About/About.jsx
@@ -5,16 +5,55 @@ import Values from './Values';
 
 import './About.css'
 
+
+const vmv = [
+  {
+    id: 1,
+    title: "Vision",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia laborum pariatur eaque nihil doloribus aliquid enim soluta, id sit repellendus minima exercitationem aperiam esse fugit quam itaque quasi, suscipit similique!",
+    image: "./images/Vision.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Mission",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia laborum pariatur eaque nihil doloribus aliquid enim soluta, id sit repellendus minima exercitationem aperiam esse fugit quam itaque quasi, suscipit similique!",
+    image: "./images/Mission.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Values",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia laborum pariatur eaque nihil doloribus aliquid enim soluta, id sit repellendus minima exercitationem aperiam esse fugit quam itaque quasi, suscipit similique!",
+    image: "./images/Values.jpg",
+  },
+];
+
+
 function About() {
   return (
     <>
       <Header Banner={"./Banner/AboutBanner.jpeg"} text={"About Us"} lightText={true} />
-      <div className='about'>
-        <h1>About</h1>
+      <div className='container'>
 
-        <div className='about__values'>
-
-        </div>
+         <div className='about__values'>
+          {
+            vmv.map((prop) => {
+              
+              const { id, title, desc, image } = prop;
+              
+              return (
+                <Values
+                  key={id}
+                  title={title}
+                  desc={desc}
+                  image={image}
+                />
+              )
+          
+            })
+          }
+        </div> 
       </div>
       </>
     
--- jyotimetallics/src/About/Values.jsx
+++ jyotimetallics/src/About/Values.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Values.css'
 
-function Values() {
+function Values({ title, desc, image}) {
   return (
-      <div className='values__container container'>
-          <img alt="vision" />
+    <div className='values__container'>
+      <img
+        src={image}
+        alt="IMP"
+        className='values__image'
+      />
+
+      <div className="values__contents">
+        <h1 className='values__title'>{ title }</h1>
+        <h1 className='values__description'>{ desc }</h1>
+      </div>
           
     </div>
   )
